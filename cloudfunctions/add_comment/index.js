ba1389de6e6155cd9c689b6b4ca04dc2824b6d73
TheWayForward <59156756+TheWayForward@users.noreply.github.com>
2020-10-21 14:48:26 +0800

    test

--- cloudfunctions/add_comment/index.js
+++ cloudfunctions/add_comment/index.js
@@ -1,4 +1,3 @@
-// 云函数入口文件
 const cloud = require('wx-server-sdk')
 
 cloud.init()
@@ -9,7 +8,6 @@ const com = db.command
 exports.main = async (event, context) => {
   return await new Promise((resolve, reject) => {
     db.collection('articles_node').doc(event.taskId).update({
-      // data 传入需要局部更新的数据
       data: {
         comment: event.my_comment
       }
